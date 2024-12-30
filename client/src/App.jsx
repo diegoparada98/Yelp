7ea785b939609eee761faf95c68010d50311c33d
dparada98 <diego.parada@bestseller.com>
2024-12-30 15:26:46 -0300

    react-components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
+            <Route exact path="/restaurants/:id" element={<RestaurantDetailPage/>}/>
+          </Routes>       
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+    
   )
 }
 
